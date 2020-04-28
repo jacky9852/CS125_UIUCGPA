9047b375e47f9adb8a45e8c966fc4cd8d75d36a0
jacky9852 <soomin2@illinois.edu>
2020-04-28 00:03:44 -0500

    Implemented average gpa button on main page

---
+++ app/src/main/java/com/example/cs125_uiucgpa/AverageGpaActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs125_uiucgpa;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AverageGpaActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.average_gpa);
+
+    }
+}
